aeed9f646d9552c6e60b88d340a3286a5752306e
Felipe Jacobucci <gotakicai@gmail.com>
2020-09-04 18:25:14 -0300

    StartPrimeiraTela
    Primeira tela desenhada em Bootstrap Studio

---
+++ Exportado/assets/js/novoProjeto.js
@@ -0,0 +1,26 @@
+function calcEstimativa(){
+    
+        var resto = 0;
+        var emDias = '';
+		var tempo = document.getElementById('estimativa').value;
+        console.log('VAI OU NAO');
+        console.log('Tempo Digitado: ' + tempo);
+		
+		resto = tempo%40;
+		console.log('resto: ' + resto);
+		
+		if(resto > 0){
+			if(tempo/40 > 0){
+				emDias = parseInt(tempo/40) + ' semana(s) e ' + Math.ceil(resto/8) + ' dias';
+			}
+		}	
+		else if(tempo /40 > 0){
+			emDias = tempo/40 + ' semana(s)';
+				
+		}
+		
+		console.log('Dias calculados: ' + emDias);
+		
+		document.getElementById('labelDias').innerHTML = emDias;
+    
+}
